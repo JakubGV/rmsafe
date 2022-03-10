9e8046cf5dcacb702a4edadd0edfa4cab896a186
jyi-jakubgvogel <kubi.vogel7@hotmail.com>
2022-03-09 19:10:33 -0500

    Define camera model class to evaluate videos

--- backend/camera_model.py
+++ backend/camera_model.py
@@ -1,3 +1,52 @@
+# Imports
+from random import randrange
+from hidden_prints import HiddenPrints
+import numpy as np
+import mxnet as mx
+from mxnet import gluon, nd, image
+from mxnet.gluon.data.vision import transforms
+from gluoncv.data.transforms import video
+from gluoncv import utils
+from gluoncv.model_zoo import get_model
+from gluoncv.utils.filesystem import try_import_decord
+
 class CameraModel:
-    def __init__(self):
-        self.test = 'hello world'
+    __classes = {
+        0: 'other',
+        1: 'falling'
+    }
+
+    def __init__(self, param_file: str): # Load model with the parameters from parameter_file
+        self.decord = try_import_decord()
+        with HiddenPrints():
+            self.net = get_model(name='i3d_resnet50_v1_custom', nclass=2)
+            self.net.load_parameters(param_file)
+
+    def get_class(self, label_num: int) -> str: # return the string label of a number class
+        if label_num in self.__classes:
+            return self.__classes[label_num]
+        else:
+            raise ValueError(f"Label number {label_num} is not valid")
+
+    def _preprocess(self, video_file): # preprocess the video for running
+        # pretend here that `video_file` is a path to a video
+        random_frame = randrange(0, 16)
+        vr = self.decord.VideoReader(video_file)
+        frame_id_list = range(random_frame, random_frame + 64, 2)
+        video_data = vr.get_batch(frame_id_list).asnumpy()
+        clip_input = [video_data[vid, :, :, :] for vid, _ in enumerate(frame_id_list)]
+        transform_fn = video.VideoGroupValTransform(size=224, mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        clip_input = transform_fn(clip_input)
+        clip_input = np.stack(clip_input, axis=0)
+        clip_input = clip_input.reshape((-1,) + (32, 3, 224, 224))
+        clip_input = np.transpose(clip_input, (0, 2, 1, 3, 4))
+
+        return clip_input
+    
+    def evaluate(self, video_file): # Run the model on the video passed and return a label
+        clip_input = self._preprocess(video_file)
+        pred = self.net(nd.array(clip_input))
+        label_num = nd.topk(pred)[0].astype('int')
+        confidence = nd.softmax(pred)[0][label_num[0]].asscalar()
+
+        return label_num[0], confidence
--- backend/rmsafe.py
+++ backend/rmsafe.py
@@ -1,6 +1,13 @@
-# Flask imports and initialization
+# Imports
+import os
 from flask import Flask
+from camera_model import CameraModel
+
+# Initializations
 app = Flask(__name__)
+curr_dir = os.path.dirname(__file__)
+param_file = os.path.join(curr_dir, 'params', 'model_07_epochs.params')
+camera_model = CameraModel(param_file)
 
 # Define API resources
 @app.route("/")
@@ -14,3 +21,10 @@ def video():
 @app.route("/video/label", methods=['GET'])
 def video_label():
     return "Video label get method"
+
+@app.route("/test", methods=['GET'])
+def test():
+    video_file = 'C:/My-Projects/gluon-tuning/test_videos/test_2_orig.mp4'
+    result, confidence = camera_model.evaluate(video_file)
+
+    return f"The model is  {confidence * 100:.1f}% sure the label is: {result}"
