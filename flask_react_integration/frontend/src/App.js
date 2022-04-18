4ce45355e53ef7e155b7af7b2ee55929094dd19a
shahirg <shahir13579@gmail.com>
2022-04-18 00:18:03 -0400

    add spacing

--- flask_react_integration/frontend/src/App.js
+++ flask_react_integration/frontend/src/App.js
@@ -21,16 +21,18 @@ function App() {
       ></script>
       <script>var Alert = ReactBootstrap.Alert;</script>
       <CustomNavbar />
-      <p>
-        RMSafe is a remote monitoring system designed for senior citizens to
-        help keep them safe within their own homes. Our system utilizes image
-        and video recognition using machine learning with cameras alongside
-        vibration sensors for more private parts of the home. Our product's
-        primary goal is to monitor the user's health, mainly whether the user
-        falls or not. The system will interact with the user through a webpage
-        on which sensor and video data are monitored and ways to contact medical
-        personnel are provided.
-      </p>
+      <div className="page-container">
+        <p>
+          RMSafe is a remote monitoring system designed for senior citizens to
+          help keep them safe within their own homes. Our system utilizes image
+          and video recognition using machine learning with cameras alongside
+          vibration sensors for more private parts of the home. Our product's
+          primary goal is to monitor the user's health, mainly whether the user
+          falls or not. The system will interact with the user through a webpage
+          on which sensor and video data are monitored and ways to contact
+          medical personnel are provided.
+        </p>
+      </div>
     </div>
   );
 }
